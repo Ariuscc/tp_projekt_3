bee7c4ac4e74e0e606ab904f301cd1141ed64839
Ariuscc <129692083+Ariuscc@users.noreply.github.com>
2023-05-15 18:46:57 +0200

    zmieniony cel startowy

--- PlanarQuadrotor/simulate.cpp
+++ PlanarQuadrotor/simulate.cpp
@@ -55,7 +55,7 @@ int main(int argc, char* args[])
      * For implemented LQR controller, it has to be [x, y, 0, 0, 0, 0]
     */
     Eigen::VectorXf goal_state = Eigen::VectorXf::Zero(6);
-    goal_state << 1100, 50, 0, 0, 0, 0;
+    goal_state << 600, 200, 0, 0, 0, 0;
     quadrotor.SetGoal(goal_state);
     /* Timestep for the simulation */
     const float dt = 0.001;
